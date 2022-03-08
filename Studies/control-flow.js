2132ef31f7dd47816f52c33a73c06db07a770aa0
Rachel Tulin <rachel.tulin@gmail.com>
2022-03-08 21:51:47 +0000

    updated

---
+++ Studies/control-flow.js
@@ -0,0 +1,21 @@
+/**
+* 0. Control flow is the order in which the computer executes statements in a script.
+* It refers to how we can make decisions in our code by asking true or false questions.
+*
+* For example, if you are developing an e-commerce application, you may want to ask a
+* user to enter a credit card number until a valid credit card is entered. On the other
+* hand, you may want your script to loop a specific number of times though a task before
+* moving on to the next part of the script. All of this logic and control flow is achieved
+* using some simple structures. These are: 
+*/
+
+// if - Statements //
+// Use the if statement to specify a block of code to be executed if a condition is true.
+if (condition) {
+  //  block of code to be executed if the condition is true
+}
+
+// example
+if (5 > 1){
+    console.log('true'); // => prints true
+}
--- Studies/variables.js
+++ Studies/variables.js
@@ -10,5 +10,26 @@
 * 1. To create or declare a variable, we use the var keyword, and followed by the
 * name of the variable that we chose. 
 *
-* 2. 
+* 2. There are two phases of using variables: declaration and initialization. 
 */
+
+// 1. Decleration //
+// To use a variable, you have to create it first. Here we are creating one variable
+// called myName. When you console log it, it will print undefined because it currently
+// has no value.  
+var myName;
+console.log(myName);// prints => undefined
+
+// 2. Initialization //
+// Once we declared our variable, you can initialize it by assigning it to a value. You can
+// do this by typing a variable name followed by an equals sign (=), followed by the value 
+// you want to give it. Don't forget a semi colon at the end of the expression. 
+myName = 'Mary';
+console.log(myName); // prints => Mary
+
+// 3. Re-assignment
+// After a variable has been initialized with a value, you can update a value by giving it a 
+// different value.
+myName = 'Maddy';
+console.log(myName); // prints => Maddy
+
